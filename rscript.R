7349768730f35a272002f0c2a300dd9966d945de
helenmoor <helenmoor@gmx.ch>
2018-10-12 12:21:50 +0200

    staged and commited rscript.R and .gitignore; did not stage the Rproj file itself
    rscript.R reads feline-data.csv and cleans it (stores cleaned data in new folder cleaned-data/feline-data-c.csv)

---
+++ rscript.R
@@ -0,0 +1,24 @@
+# read in cats data
+cats <- read.csv(file = "data/feline-data.csv")#, stringsAsFactors = F) # could read stringsAsFactor=F
+# typeof(cats$coat) #  determines the (R internal) type or storage mode  -- is integer
+# str(cats)  # factor
+# typeof(cats$likes_string) # could convert this to logical
+
+# typeof(TRUE) # logical
+# typeof(3.14) # double
+# typeof(3) # double -> to really get integer: type 3L
+# typeof(3L) # integer
+# typeof(1+1i) # complex
+
+# vector1 <- vector(length=3) # creates logical
+# vector2 <- vector(mode = "character", length=3) # 3 empty character type elements
+
+# basic r material for teaching online (teaching: show, make them do, repeat, find out themselves...)
+
+# in between work on cats data, cleaning data (keep raw data as is)
+
+if(!dir.exists("clean_data")){  # if doesnt exist yet, create dir
+    dir.create("clean_data", showWarnings = FALSE)
+}
+write.csv(cats, file = "clean_data/feline-data-c.csv", row.names = FALSE)
+
